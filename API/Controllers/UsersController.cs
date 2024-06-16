1b5450240030eaaf83ab84fe56f417baa1b8cb28
CORP\Raul.PETCU <raul.petcu@geminicad.com>
2024-06-16 20:23:52 +0300

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _dbContext;
+
+    public UsersController(DataContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _dbContext.Users.ToListAsync();
+
+        return users; 
+    }
+
+    [HttpGet("{id}")] // /api/users/3
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _dbContext.Users.FindAsync(id);
+    }
+}
